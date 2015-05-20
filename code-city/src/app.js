7b579782717df81c0bd9244c00d7b672e7741389
Jarda Snajdr <jsnajdr@gmail.com>
2015-05-20 17:17:16 +0200

    Added 3D rendering in three.js

--- code-city/src/app.js
+++ code-city/src/app.js
@@ -1,15 +1,16 @@
 function legend_function(d) {
-    return "<h2>" + (d.path || "/") + "</h2><p>" + d.value + " lines of code.</p>"
+    return "<h2>" + (d.path || "/") + "</h2><p>" + d.lines + " lines of code.</p>"
 }
 
-function initChart(chartEl, legendEl) {
-    var diameter = 800,
+function initChart(chartEl, legendEl, scene) {
+    var diameter = 1000,
         format = d3.format(",d"),
         color = d3.scale.category20c();
 
     var bubble = d3.layout.pack()
         .sort(null)
         .size([diameter, diameter])
+        .value(function(d) { return 1; })
         .padding(1.5);
 
     var svg = d3.select(chartEl).append("svg")
@@ -19,16 +20,17 @@ function initChart(chartEl, legendEl) {
 
     d3.json("metrics.json", function(error, root) {
       	var list = root.summary.python.metrics;
-      	var tree = treeize(list);
+      	var tree = treeize(list, 2);
         var nodes = bubble.nodes(tree);
         var node = svg.selectAll(".node")
           .data(nodes)
           .enter().append("g")
             .attr("class", "node")
-            .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+            .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+            .each(function(d) { addHouse(scene, d.x, d.y, d.r, d.depth, color(d.depth)); });
 
         node.append("title")
-            .text(function(d) { return d.path + ": " + format(d.value); });
+            .text(function(d) { return d.path + ": " + format(d.lines); });
 
         node.append("circle")
             .attr("r", function(d) { return d.r; })
@@ -49,28 +51,69 @@ function initChart(chartEl, legendEl) {
                 legend.transition().duration(1000).style("opacity","0");
             }
         }
-    // node.append("text")
-    //     .attr("dy", ".3em")
-    //     .style("text-anchor", "middle")
-    //     .text(function(d) { return d.name.substring(0, d.r / 3); });
     });
-  
-  //d3.select(self.frameElement).style("height", diameter + "px");
 }
 
-function treeize(list) {
+function addHouse(scene, x, y, r, level, color) {
+  console.log('adding house', x, y, r, level);
+  var geometry = new THREE.CylinderGeometry( r / 500, r / 500, 0.1, 64 );
+  var material = new THREE.MeshLambertMaterial( { color: color } );
+  var cube = new THREE.Mesh( geometry, material );
+  
+  cube.rotation.x = 1.57;
+  cube.position.x = x / 500 - 1;
+  cube.position.y = y / 500 - 1;
+  cube.position.z = level * 0.1;
+  
+  cube.castShadow = true;
+
+  scene.add( cube );
+}
+
+function initScene(canvasEl) {
+  var canvas = d3.select(canvasEl);
+  var scene = new THREE.Scene();
+  var camera = new THREE.PerspectiveCamera( 45, 1, 0.1, 1000 );
+
+  var renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+  renderer.setSize( 1000, 1000 );
+  renderer.setClearColor( 0xffffff, 1);
+  canvas.node().appendChild(renderer.domElement);
+
+  camera.position.y = -2;
+  camera.position.z = 2;
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+  var directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1.0);
+  directionalLight.position.set(-10, -20, 40);
+  scene.add(directionalLight);
+  
+  // add subtle ambient lighting
+  var ambientLight = new THREE.AmbientLight(0x313131);
+  scene.add(ambientLight);
+  
+  function render(timestamp) {
+  	requestAnimationFrame( render );
+    renderer.render( scene, camera );
+  }
+  render();
+  
+  return scene;
+}
+
+function treeize(list, maxLevel) {
   var tree = {};
   
   for (path in list) {
     if (list.hasOwnProperty(path)) {
-      addChild(tree, path.split('/'), path, list[path]);
+      addChild(tree, path.split('/'), path, list[path], maxLevel);
     }
   }
   
   return tree;
 }
 
-function addChild(tree, nameList, path, node) {
+function addChild(tree, nameList, path, node, maxLevel) {
   var nameFirst = nameList.shift();
   var child;
 
@@ -92,16 +135,18 @@ function addChild(tree, nameList, path, node) {
     }
 
     if (nameList.length > 0) {
-      addChild(child, nameList, path, node);
+      if (maxLevel === undefined || (--maxLevel >= 0)) {
+        addChild(child, nameList, path, node, maxLevel);
+      }
     }
     else {
       child.path = path;
-      child.value = node.total_number_of_lines;
+      child.lines = node.total_number_of_lines;
     }
   }
   else {
     tree.name = nameFirst;
     tree.path = path;
-    tree.value = node.total_number_of_lines;
+    tree.lines = node.total_number_of_lines;
   }
 }
