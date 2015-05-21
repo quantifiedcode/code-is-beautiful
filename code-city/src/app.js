e24c9d29909b50a1fd23cb71f81b344a3ba39301
Jarda Snajdr <jsnajdr@gmail.com>
2015-05-21 18:13:58 +0200

    Show legend on 3D view mouseover

--- code-city/src/app.js
+++ code-city/src/app.js
@@ -20,14 +20,14 @@ function initChart(chartEl, legendEl, scene) {
 
     d3.json("metrics.json", function(error, root) {
       	var list = root.summary.python.metrics;
-      	var tree = treeize(list, 1);
+      	var tree = treeize(list);
         var nodes = bubble.nodes(tree);
         var node = svg.selectAll(".node")
           .data(nodes)
           .enter().append("g")
             .attr("class", "node")
             .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
-            .each(function(d) { scene.addHouse(d.x, d.y, d.r, d.depth, color(d.depth)); });
+            .each(function(d) { scene.addHouse(d, d.x, d.y, d.r, d.depth, color(d.depth)); });
 
         node.append("title")
             .text(function(d) { return d.path + ": " + format(d.lines); });
@@ -37,47 +37,88 @@ function initChart(chartEl, legendEl, scene) {
             .style("fill", function(d) { return color(d.depth); });
 
         if (legendEl) {
-            node.on("mouseover",update_legend)
-                .on("mouseout",remove_legend);
-  
-            var legend = d3.select(legendEl);
+          var legend = d3.select(legendEl);
 
-            function update_legend(d) {
-                legend.html(legend_function(d));
-                legend.transition().duration(200).style("opacity","1");
-            }
+          function update_legend(d) {
+              legend.html(legend_function(d));
+              legend.transition().duration(200).style("opacity","1");
+          }
 
-            function remove_legend(d) {
-                legend.transition().duration(1000).style("opacity","0");
-            }
+          function remove_legend(d) {
+              legend.transition().duration(1000).style("opacity","0");
+          }
+
+          node.on("mouseover",update_legend)
+            .on("mouseout",remove_legend);  
         }
         
-        scene.render();
+        scene.animate();
     });
 }
 
-function addHouse(scene, x, y, r, level, color) {
-  console.log('adding house', x, y, r, level);
-  var geometry = new THREE.CylinderGeometry( r / 500, r / 500, 0.1, 64 );
-  var material = new THREE.MeshLambertMaterial( { color: color } );
-  var cube = new THREE.Mesh( geometry, material );
-  
-  cube.rotation.x = 1.57;
-  cube.position.x = x / 500 - 1;
-  cube.position.y = y / 500 - 1;
-  cube.position.z = level * 0.1;
-  
-  cube.castShadow = true;
-
-  scene.add( cube );
-}
-
-function initScene(canvasEl) {
+function initScene(canvasEl, legendEl) {
   var canvas = d3.select(canvasEl);
+  var mouse = new THREE.Vector2();
+  var raycaster = new THREE.Raycaster();
   var scene = new THREE.Scene();
   var camera = new THREE.PerspectiveCamera( 45, 1, 0.1, 1000 );
-
   var renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+  var intersected;
+  var legend = d3.select(legendEl);
+
+  function update_legend(d) {
+      legend.html(legend_function(d));
+      legend.transition().duration(200).style("opacity","1");
+  }
+
+  function remove_legend(d) {
+      legend.transition().duration(1000).style("opacity","0");
+  }
+  
+  function render() {
+		raycaster.setFromCamera(mouse, camera);
+
+		var intersects = raycaster.intersectObjects( scene.children );
+
+		if (intersects.length > 0) {
+			if (intersected != intersects[0].object) {
+				if (intersected) {
+          intersected.material.emissive.setHex(intersected.currentHex);
+        }
+      
+				intersected = intersects[ 0 ].object;
+				intersected.currentHex = intersected.material.emissive.getHex();
+				intersected.material.emissive.setHex( 0xff0000 );
+        
+        update_legend(intersected.d);
+			}
+
+		}
+    else if (intersected) {
+      intersected.material.emissive.setHex(intersected.currentHex);
+			intersected = null;      
+      remove_legend();
+		}
+  
+    renderer.render(scene, camera);
+  }
+  
+  function animate() {
+    requestAnimationFrame(animate);
+    
+    render();
+  }
+  
+  function onCanvasMouseMove(event) {
+    var e = event;
+    var et = e.target;
+  
+    e.preventDefault();
+  
+    mouse.x = ((e.pageX - et.offsetLeft) / et.clientWidth) * 2 - 1;
+    mouse.y = - ((e.pageY - et.offsetTop) / et.clientHeight) * 2 + 1;
+  }
+
   renderer.setSize( 1000, 1000 );
   renderer.setClearColor( 0xffffff, 1);
   canvas.node().appendChild(renderer.domElement);
@@ -93,30 +134,28 @@ function initScene(canvasEl) {
   // add subtle ambient lighting
   var ambientLight = new THREE.AmbientLight(0x313131);
   scene.add(ambientLight);
-    
+
+  renderer.domElement.addEventListener('mousemove', onCanvasMouseMove, false);
+
   return {
-    addHouse: function(x, y, r, level, color) {
-      var geometry = new THREE.CylinderGeometry( r / 500, r / 500, 0.1, 64 );
+    addHouse: function(d, x, y, r, level, color) {
+      var segments = Math.min(Math.max(Math.floor(r), 8), 100);
+      var geometry = new THREE.CylinderGeometry( r / 500, r / 500, 0.1, segments);
       var material = new THREE.MeshLambertMaterial( { color: color } );
       var cube = new THREE.Mesh( geometry, material );
   
-      cube.rotation.x = 1.57;
+      cube.rotation.x = Math.PI / 2;
       cube.position.x = x / 500 - 1;
       cube.position.y = y / 500 - 1;
       cube.position.z = level * 0.1;
   
       cube.castShadow = true;
 
+      cube.d = d;
       scene.add( cube );
     },
     
-    render: function() {
-      renderer.render( scene, camera );
-
-      function animate() {
-      	requestAnimationFrame(animate);    
-      }
-
+    animate: function() {
       animate();
     }
   };
