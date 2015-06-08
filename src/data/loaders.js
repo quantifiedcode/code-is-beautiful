a191715e7aafd3268ab65109daff2f01030d7ff9
Andreas Dewes <andreas.dewes@gmail.com>
2015-06-08 14:31:03 +0200

    Fixed data link.

--- src/data/loaders.js
+++ src/data/loaders.js
@@ -1,7 +1,7 @@
 define(["data/helpers"],function(dataHelpers){
     return {
         complexityExample : function(){
-            var data = dataHelpers.loadJson('/data/django_summary.json');
+            var data = dataHelpers.loadJson('../data/django_summary.json');
             return data;
         },
     }
