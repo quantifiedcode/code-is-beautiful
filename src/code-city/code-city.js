a17437fee1d09c091bf08df54a45e12210109418
Andreas Dewes <andreas.dewes@gmail.com>
2015-06-09 13:49:59 +0200

    Many improvements for code city chart.

--- src/code-city/code-city.js
+++ src/code-city/code-city.js
@@ -70,10 +70,58 @@ function(){
 
           var raycaster = new THREE.Raycaster();
           var scene = new THREE.Scene();
-          var camera = new THREE.OrthographicCamera(-1.5, 1.5, 1.2, -1.2, 0.2, 1000 );
+          var camera = new THREE.PerspectiveCamera(45.0, width/height, 1.0, 1000 );
+
+          camera.position.y = -3;
+          camera.position.z = 2;
+          camera.lookAt(new THREE.Vector3(0, -0.25, 0));
+
           var renderer;
           var intersected;
           var rootHouse;
+
+          var fragmentShader = " \
+varying vec2 vUv; \
+varying vec3 vColor; \
+varying vec3 pos; \
+uniform float glow; \
+void main() { \
+    float m = 0.8+pos.z*1.0; \
+    float d1 = abs(vUv.x-1.0); \
+    float d2 = vUv.x; \
+    float d3 = abs(vUv.y-1.0); \
+    float d4 = vUv.y; \
+    float minDistance = min(min(min(d1,d2),d3),d4); \
+    float dEdge = 1.0; \
+    float treshold = 0.12; \
+    float multiplier = 1.05; \
+    if (minDistance < treshold) \
+        dEdge = multiplier/(1.0+pow(minDistance/treshold+0.01,2.0)*(multiplier-1.0)); \
+    m = dEdge*m; \
+    m = m*glow; \
+    float dPersp = sqrt(pow(vUv.x-1.0,2.0)+pow(vUv.y-1.0,2.0)); \
+    m = (1.0+dPersp*0.3)*m; \
+    gl_FragColor = vec4(vColor.x*m, vColor.y*m,vColor.z*m ,1.0); \
+}";
+          var vertexShader = " \
+uniform float glow; \
+uniform vec3 color; \
+varying vec3 vColor; \
+varying vec2 vUv; \
+varying vec3 pos; \
+void main() { \
+    vUv = uv; \
+    vColor = color; \
+    gl_Position = projectionMatrix * \
+                  modelViewMatrix * vec4(position, 1.0 ); \
+    pos = position; \
+} \
+";
+
+          var cameraDistance = 3.0;
+          var cameraHeight = 1.8;
+          var cameraZ = -1.5;
+          var cameraAngle = 0.0;
 
           function addHouse(d){
             var unitHeight = 3 / 1000;
@@ -81,15 +129,25 @@ function(){
             var h = 1000;
             var gw = Math.max(0, (d.dx - 2 * houseMargin)*w) / 500;
             var gh = Math.max(0, (d.dy - 2 * houseMargin)*h) / 500;
-            var gd = unitHeight * (d.children ? 1 : d.height);
+            var gd = unitHeight * (d.children ? 1 : (d.height-rawData.minima.height)/(rawData.maxima.height-rawData.minima.height)*500.0);
 
             var gx = ((d.x + d.dx/2)*w ) / 500 - 1;
             var gy = 1 - ((d.y + d.dy/2)*h ) / 500;
             var gz = d.depth * unitHeight + gd / 2;
 
+            var shaderMaterial = new THREE.ShaderMaterial({
+                fragmentShader: fragmentShader,
+                vertexShader: vertexShader,
+                uniforms : {
+                    color : {type : 'c',value: new THREE.Color(d.color)},
+                    glow : {type: 'f',value : 1.0},
+                }
+            });
+
+
             var geometry = new THREE.BoxGeometry(gw, gh, gd);
             var material = new THREE.MeshLambertMaterial({ color: d.color });
-            var cube = new THREE.Mesh(geometry, material);
+            var cube = new THREE.Mesh(geometry, shaderMaterial);
 
             cube.position.x = gx;
             cube.position.y = gy;
@@ -138,12 +196,13 @@ function(){
             if (intersects.length > 0) {
               if (intersected != intersects[0].object || true) {
                 if (intersected) {
-                  intersected.material.emissive.setHex(intersected.currentHex);
+                  intersected.material.uniforms.glow = {type : 'f',value : 1.0};
+                  intersected.material.needsUpdate = true;
                 }
 
                 intersected = intersects[0].object;
-                intersected.currentHex = intersected.material.emissive.getHex();
-                intersected.material.emissive.setHex( 0xff0000 );
+                intersected.material.uniforms.glow = {type : 'f',value : 1.4};
+                intersected.material.needsUpdate = true;
 
                 selectedD = intersected.d;
                 if (params.legend){
@@ -153,16 +212,18 @@ function(){
               }
             }
             else if (intersected) {
-              intersected.material.emissive.setHex(intersected.currentHex);
+                intersected.material.uniforms.glow = {type : 'f',value : 1.0};
+                intersected.material.needsUpdate = true;
               intersected = null;
 
               if (params.legend && selectedD)
                   params.legend.onMouseout(selectedD,e);
 
               selectedD = undefined;
-
               render();
+
             }
+
           }
 
           function onCanvasMouseClick(e){
@@ -178,20 +239,16 @@ function(){
 
           canvas.node().appendChild(renderer.domElement);
 
-          camera.position.y = -3;
-          camera.position.z = 2;
-          camera.lookAt(new THREE.Vector3(0, -0.25, 0));
-
           var directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1.0);
           directionalLight.position.set(-5, -10, 15);
           directionalLight.castShadow = true;
           directionalLight.shadowCameraNear = 0.01;
-          directionalLight.shadowCameraFar = 40;
+          directionalLight.shadowCameraFar = 20;
           directionalLight.shadowCameraRight = 1.5;
           directionalLight.shadowCameraLeft = -1.5;
           directionalLight.shadowCameraTop  = 1.5;
           directionalLight.shadowCameraBottom = -1.5;
-          directionalLight.shadowDarkness = 0.4;
+          directionalLight.shadowDarkness = 0.5;
 
           scene.add(directionalLight);
 
@@ -206,6 +263,46 @@ function(){
 
           render();
           animate();
+
+          var distance = 500.0;
+          var maxHeight = -20.0;
+          var maxDistance = distance;
+          var distanceAngle = 10.0;
+
+          var flyBy = function(){
+              var acceleration = Math.pow((maxDistance-distance)/maxDistance,2.0);
+              distance -= 0.01+8.0*(1.0-acceleration);
+              var height = distance/maxDistance*maxHeight;
+              distanceAngle=distance*0.1;
+
+              camera.position.set(-(distance+cameraDistance)*Math.cos(distanceAngle+cameraAngle),-(distance+cameraDistance)*Math.sin(distanceAngle+cameraAngle),height+cameraHeight);
+              camera.up = new THREE.Vector3(0,0,1);
+              camera.lookAt(new THREE.Vector3(Math.cos(cameraAngle+distanceAngle),Math.sin(cameraAngle+distanceAngle),cameraZ));
+
+              render();
+              if (distance > 0)
+                setTimeout(flyBy,10);
+          };
+
+          flyBy();
+
+          var setCameraRotation = function(angle){
+              cameraAngle = angle;
+              camera.position.set(-cameraDistance*Math.cos(cameraAngle),-cameraDistance*Math.sin(cameraAngle),cameraHeight);
+              camera.up = new THREE.Vector3(0,0,1);
+              camera.lookAt(new THREE.Vector3(Math.cos(cameraAngle),Math.sin(cameraAngle),cameraZ));
+              render();
+          }
+
+          var getCameraRotation = function(){
+            return cameraAngle;
+          }
+
+          return {
+            getCameraRotation : getCameraRotation,
+            setCameraRotation : setCameraRotation
+          };
+
         }
 
         exports.initChart = initChart;
